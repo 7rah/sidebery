a9a78bb56a02d31306e7aa23dcfa6b51202822ad
zrah <zrah@protonmail.com>
2026-03-28 20:12:42 +0800

    fix: restore chrome tab trees after restart

--- src/services/tabs.fg.actions.ts
+++ src/services/tabs.fg.actions.ts
@@ -531,7 +531,6 @@ function findCachedData(
   if (Windows.uniqWinId && Windows.uniqWinId !== NOID) {
     const winTabsCache = data.find(winTabs => winTabs[0]?.uniqWinId === Windows.uniqWinId)
     if (winTabsCache) data = [winTabsCache]
-    else if (!Platform.hasSessionValues) return
   }
 
   for (const winTabs of data) {
